5a167aa3e787433899e3df28f5f61d6763e3bc09
Gohara <gohara.dev@gmail.com>
2023-06-18 13:12:06 -0300

    Section Ruler e outros ajustes

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -15,3 +15,28 @@ document.addEventListener('scroll', fixedMenu);
 AOS.init({
     duration: 1000
 });
+
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 2,
+    speed: 8000,
+    allowTouchMove: false,
+    loop: true,
+    autoplay: {
+        delay: 1,
+    },
+    // Configurações iniciais para dispositivos móveis
+    breakpoints: {
+        // Quando a largura da tela for igual ou maior que 640px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // Quando a largura da tela for igual ou maior que 768px
+        768: {
+            slidesPerView: '10',
+            spaceBetween: 40
+        }
+        // E assim por diante para tamanhos de tela maiores
+    }
+});
